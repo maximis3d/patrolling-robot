1f12b55d2378e21d4e52d69434e1c666a0134de5
Max <marstonvisuals@gmail.com>
2025-01-13 19:04:52 +0000

    Empty

--- ros2_ws/src/patrolling_robot_simulation/setup.py
+++ ros2_ws/src/patrolling_robot_simulation/setup.py
@@ -45,6 +45,8 @@ setup(
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [],
+        'console_scripts': [
+            "patrolling_robot_node=patrolling_robot_simulation.patrolling_robot_node:main"
+        ],
     },
 )
