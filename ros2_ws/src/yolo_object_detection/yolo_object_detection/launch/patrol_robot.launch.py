a2859f6ad93ef6f0e83740d1220d4ea255742406
Max <marstonvisuals@gmail.com>
2025-01-14 14:45:35 +0000

    Edit launch file for yolo_object_detection package

--- ros2_ws/src/yolo_object_detection/yolo_object_detection/launch/patrol_robot.launch.py
+++ ros2_ws/src/yolo_object_detection/yolo_object_detection/launch/patrol_robot.launch.py
@@ -3,34 +3,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # Baseline Manager Node
-        Node(
-            package="yolo_object_detection",
-            executable="baseline_manager_node",
-            name="baseline_manager_node",
-            output="screen"
-        ),
-        
-        # Patrolling Node (YOLO Object Detection)
-        Node(
-            package="yolo_object_detection",
-            executable="patrolling_yolo_node",
-            name="patrolling_yolo_node",
-            output="screen"
-        ),
-        
-        # Patrolling Robot Node (Navigation)
-        Node(
-            package="yolo_object_detection",
-            executable="patrolling_robot_node",
-            name="patrolling_robot_node",
-            output="screen"
-        ),
-        
-        # Launch Nav2 Stack
-        Node(
-            package="nav2_bringup",
-            executable="bringup_launch.py",
-            output="screen"
-        )
+        Node(package='yolo_object_detection', executable='state_manager_node'),
+        Node(package='yolo_object_detection', executable='baseline_manager_node'),
+        Node(package='yolo_object_detection', executable='patrolling_yolo_node'),
     ])
